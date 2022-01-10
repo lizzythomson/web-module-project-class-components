f293d7801817563b5d8d4febd576e5a16d3c6d5c
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-01-10 14:48:06 -0700

    setup basic layout of app

---
+++ src/components/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ToDoForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type='text' />
+        <button>Add Another Task</button>
+      </form>
+    );
+  }
+}
+
+export default ToDoForm;
